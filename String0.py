1ce25d8e5516a00e5df4901d0b1c60a967770792
Redwan92 <49997435+Redwan92@users.noreply.github.com>
2019-04-27 03:41:56 +0600

    Add files via upload

---
+++ String0.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 16:50:30 2019
+
+@author: Anik
+"""
+
+#name = "MD Redwanul haque"
+#length = len(name) 
+#print (length)
+#print (name.lower())
+#print (name.upper())
+#print (name.title())
+#print (name.count("a"))
+
+#namen , char =input( "enter a name comma seperated character :").split(",")
+#print(f"length of your name is {len(namen)}")
+#print(f"character count is : {namen.count(char)}")
+
+name = "Md Anik "
+length =len(name) 
+print(length)
+value=" awesome said "
+print("python is"+value +name) 
+x="python is "
+y="awesome"
+z=" anik like this"
+k=x+y+z
+print (k)
+#python mumber
+x1 = 3+5j
+y1 = 5j
+z1 = -5j
+
+print(type(x1))
+print(type(y1))
+print(type(z1))
+x2 = 35e3
+y2 = 12E4
+z2 = -87.7e100
+
+print(type(x2))
+print(type(y2))
+print(type(z2))
+
+#variable
+x3 = 5
+y3 = "John"
+print(type(x3))
+print(type(y3))
+#type casting
+x4 = float(1)       # x will be 1.0
+y4 = int(2.8)       # y will be 2.8
+z4 = int("3")       # z will be 3.0
+w4 = float("4.2")   # w will be 4.2
+w4 = int("4.2")     #this is not possible 
+print(x4)
+print(y4)
+print(z4)
+print(w4)
+
